47f53ab384f5f3d0e2d919b360f97347e8c298fc
marwai <marcus.96@live.co.uk>
2021-03-01 22:30:43 +0000

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,18 @@
+class profile::ssh_server {
+    package {'openssh-server'{
+            ensure => present,
+    }
+    service { 'sshd':
+            ensure => 'running',
+            enable => 'true',
+    }
+    ssh_authorized_key { 'root@master.puppet.vm':
+            ensure => present,
+            user   => 'root',
+            type   => 'ssh-rsa',
+            key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDJjVRYuF7wzjEAwnZtg+xshfXX5YHrInsz7n3/KgNthStu/Y3J2qPQkBP25E9aBL4a
+ykeLDO+4/fO+rVtG9intfWhktd6L+nQilCZtHrDDj009neTsGgrR3Ye1v96eSbhRlrMTg4oaiedGEOm+G1vCRJrmwawFrvZVGqy5qVVD
+sn9baF1KcZ8uVrjxsTqdhFm1KqqXNf8IIPuQP74PjJC7Ea8PwSDqZjZKRj0UHSXCpIokfoSOK8Kn2/Z0+oHp/1SvpuQfT8JoPa9leMZV
+vNLPYg/qoZJ/ISOoJclsLEYtcb8NcF9w7Z2AtaQFcjDZiu1S72/qf1wKxlEasIsstrjX root@master.puppet.vm',
+    }
+}
