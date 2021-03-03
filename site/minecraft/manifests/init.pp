21ddd071dff9f52b9306a897327b202b072de714
marwai <marcus.96@live.co.uk>
2021-03-03 21:57:38 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -14,12 +14,14 @@ class minecraft (
     ensure => present,
   }
   file {"${install_dir}/eula.txt":
-    ensure => file,
+    ensure  => file,
     content => 'eula=true'
   }
   file {'/etc/systemd/system/minecraft.service':
-    ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    ensure        => file,
+    content       => epp('minecraft/minecraft.service',{
+      install_dir => $install_dir,
+    })
   }
   service {'minecraft':
     ensure  => running,
