c972f5657619182152a1d068619f973311ed2516
marwai <marcus.96@live.co.uk>
2021-03-03 20:52:27 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,23 +1,23 @@
 class minecraft {
-  file {‘/opt/minecraft’:
+  file {'/opt/minecraft':
     ensure => directory,
   }
-  file {‘/opt/minecraft/minecraft_server.1.12.2.jar’:
+  file {'/opt/minecraft/minecraft_server.1.12.2.jar':
     ensure => file,
     source      => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
   }
-  package {‘java’:
+  package {'java':
     ensure => present,
   }
-  file {‘/opt/minecraft/eula.txt’:
+  file {'/opt/minecraft/eula.txt':
     ensure => file,
-    content => ‘eula=true’
+    content => 'eula=true'
   }
-  file {‘/etc/systemd/system/minecraft.service’:
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => ‘puppet:///modules/minecraft/minecraft.service’,
+    source => 'puppet:///modules/minecraft/minecraft.service',
   }
-  service { ‘minecraft’:
+  service { 'minecraft':
     ensure => running,
     enable => true,
   }
