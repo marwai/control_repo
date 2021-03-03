fb4a784d4fa2c6c73d06621b80946f1ae5ea834b
marwai <marcus.96@live.co.uk>
2021-03-03 21:43:33 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,8 @@ class minecraft {
   }
   file {'/opt/minecraft/minecraft_server.1.12.2.jar':
     ensure => file,
-    source      => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+    source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+    before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
